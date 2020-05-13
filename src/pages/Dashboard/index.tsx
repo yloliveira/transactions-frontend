f40cc27cb6798c2b8ed2f7a897881f4ebb4be6da
Yan Oliveira <oliveira.yan02@gmail.com>
2020-05-13 19:56:45 -0300

    refactor: display transactions data

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -90,20 +90,19 @@ const Dashboard: React.FC = () => {
               </tr>
             </thead>
 
-            <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
-            </tbody>
+            {transactions.map(transaction => (
+              <tbody>
+                <tr>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {formatValue(transaction.value)}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.created_at}</td>
+                </tr>
+              </tbody>
+            ))}
           </table>
         </TableContainer>
       </Container>
